f17cdadfca2e4ffc0b9adce2be9b7e666f9a4598
Nandan kalia <99944363+nandankalia@users.noreply.github.com>
2024-01-15 13:25:05 +0530

    first

---
+++ python and django.py
@@ -0,0 +1,55 @@
+# Built-in Data Types
+
+# Text Type     :	str
+
+# x = "Hello World"
+# print (x)
+
+# Numeric Types :	int, float, complex
+
+
+# #int
+
+# x = 20
+# print (x)
+
+#float
+
+# x = 20.0
+# print (x)
+
+#complex
+
+# x = 1j
+# print (x)
+
+# Sequence Types:	list, tuple, range
+
+#list []
+
+# x = ["green", "blue", "white"]
+# print (x)
+
+#Tuple ()
+
+# x = ("green", "blue", "white")
+# print (x)
+
+
+#range
+
+# for i in range(5):
+#     print(i)
+
+
+# Mapping Type  :	dict
+
+
+
+
+
+
+# Set Types     :	set, frozenset
+# Boolean Type  :	bool
+# Binary Types  :	bytes, bytearray, memoryview
+# None Type     :	NoneType
