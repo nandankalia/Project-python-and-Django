5c04651b8ced4eb3a992b9f49a1fc4fd06f5ad4f
Nandan kalia <99944363+nandankalia@users.noreply.github.com>
2024-01-16 12:31:55 +0530

    3rd message

--- python and django.py
+++ python and django.py
@@ -62,3 +62,100 @@
 # print("cat" in animals) 
 # print("elephant" in animals)
 
+#Python Operators
+#################################### Arithmetic operators
+
+
+#                                   +	    Addition	    x + y
+
+#                                          print(20+30)
+
+#                                     -	    Subtraction	    x - y	
+
+                                           #print(30-20)
+                                             
+#                                   *	    Multiplication	x * y	
+
+                                            #print(3*2)
+
+
+#                                     /	    Division	    x / y	
+
+#The Python code print(10/3) will output the result of the regular division operation, which includes the remainder. 
+#print(10/3)
+  #output = 3.3333333333333335
+
+
+#                                     %	    Modulus	        x % y
+
+#the % operator is used for the modulo operation, which calculates the remainder when one number is divided by another.
+
+
+#                                     **	Exponentiation	x ** y	
+
+#In Python, the ** operator is used for exponentiation. If you have variables x and y, x ** y calculates x raised to the power of y 
+                                               #print(2**3)
+                                             #output is 8
+
+#                                      //	Floor division	x // y	
+
+#The Python code print(10//3) will output the result of the floor division operation, which is the quotient without the remainder. 
+#print(10//3)
+  # output = 3
+
+# Assignment operators  # assignment operators are used to assign values to variables #
+
+# =: Assigns the value on the right to the variable on the left.
+  
+# x = 5
+
+#  +=: Adds the value on the right to the current value of the variable on the left.
+
+# x = 5
+# y = 3
+# x += y  # This is equivalent to x = x + y
+# print(x)  # Output: 8
+
+# a = 5
+# b = 3
+# # a = a - b 
+# a -= b 
+# print(a)
+## Output = 2
+
+# a = 3
+# b = 5
+  # # a = a * b 
+# a *= b 
+   # print(a)
+# # Output = 15
+
+
+
+# a = 3
+# b = 5
+  # # a = a / b 
+# a /= b 
+  # print(a)
+# # Output = 0.6
+
+
+# a = 3
+# b = 5
+  
+# # a = a % b 
+# a %= b 
+  # print(a)
+# # Output 3
+
+# a = 3
+# b = 5
+  
+# # a = a ** b 
+# a **= b 
+#   print(a)
+# Output = 243
+
+
+
+
